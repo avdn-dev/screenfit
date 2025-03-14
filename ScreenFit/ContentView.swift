4b9867e6dfe1e80266c1769cd4dd793225c0854c
Anh Nguyen <121279445+avdn-dev@users.noreply.github.com>
2025-03-14 18:26:12 +1100

    Initialise project

---
+++ ScreenFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenFit
+//
+//  Created by Anh Nguyen on 14/3/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenFit/ScreenFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenFitApp.swift
+//  ScreenFit
+//
+//  Created by Anh Nguyen on 14/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

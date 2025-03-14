4fcae3b2dd91703e3580d4a8359d4091e20dadcf
Anh Nguyen <121279445+avdn-dev@users.noreply.github.com>
2025-03-14 18:35:28 +1100

    Add FamilyControls authorisation request

--- ScreenFit/ScreenFitApp.swift
+++ ScreenFit/ScreenFitApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 @main
 struct ScreenFitApp: App {
+    let center = AuthorizationCenter.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(perform: requestFamilyControlsAuthorisation)
+        }
+    }
+    
+    private func requestFamilyControlsAuthorisation() {
+        Task {
+            do {
+                try await center.requestAuthorization(for: .individual)
+            } catch {
+                print("Failed to request Screen Time authorization: \(error)")
+            }
         }
     }
 }

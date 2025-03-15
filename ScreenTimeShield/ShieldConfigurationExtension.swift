03c2b0e63fe7968c7b3545acc425f5a0dcf8b1c4
Anh Nguyen <121279445+avdn-dev@users.noreply.github.com>
2025-03-16 10:13:32 +1100

    Customise screen time shield with dark mode, app icon and accent colour

--- ScreenTimeShield/ShieldConfigurationExtension.swift
+++ ScreenTimeShield/ShieldConfigurationExtension.swift
@@ -13,25 +13,21 @@ import UIKit
 // The system provides a default appearance for any methods that your subclass doesn't override.
 // Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
 class ShieldConfigurationExtension: ShieldConfigurationDataSource {
-    let backgroundBlurStyle: UIBlurEffect.Style = .systemChromeMaterial
-    let backgroundColor: UIColor = .systemPurple.withAlphaComponent(0.1)
-    let icon: UIImage = UIImage(systemName: "lock.badge.clock.fill")!
-    let titleColor: UIColor = .label
-    let subtitleColor: UIColor = .secondaryLabel
-    let primaryButtonLabelColor: UIColor = .label
-    let primaryButtonBackgroundColor: UIColor = .systemPurple
+    let backgroundBlurStyle: UIBlurEffect.Style = .systemChromeMaterialDark
+    let icon: UIImage = UIImage(named: "ScreenFitCutOut")!
+    let textColor: UIColor = .label.resolvedColor(with: .init(userInterfaceStyle: .dark))
+    let primaryButtonLabelColor: UIColor = .label.resolvedColor(with: .init(userInterfaceStyle: .dark))
+    let primaryButtonBackgroundColor: UIColor = UIColor(named: "AccentColor")!
     
     override func configuration(shielding application: Application) -> ShieldConfiguration {
         // Customize the shield as needed for applications.
         ShieldConfiguration(
             backgroundBlurStyle: backgroundBlurStyle,
-            backgroundColor: backgroundColor,
             icon: icon,
-            title: .init(text: "\(application.localizedDisplayName!) blocked by ScreenFit", color: titleColor),
-            subtitle:  .init(text: "Open ScreenFit and do some exercise to unblock.", color: subtitleColor),
+            title: .init(text: "\(application.localizedDisplayName!) blocked by ScreenFit", color: textColor),
+            subtitle:  .init(text: "Open ScreenFit and do some exercise to unblock.", color: textColor),
             primaryButtonLabel: .init(text: "OK", color: primaryButtonLabelColor),
-            primaryButtonBackgroundColor: primaryButtonBackgroundColor,
-            secondaryButtonLabel: nil
+            primaryButtonBackgroundColor: primaryButtonBackgroundColor
         )
     }
     
@@ -39,13 +35,11 @@ class ShieldConfigurationExtension: ShieldConfigurationDataSource {
         // Customize the shield as needed for applications shielded because of their category.
         ShieldConfiguration(
             backgroundBlurStyle: backgroundBlurStyle,
-            backgroundColor: backgroundColor,
             icon: icon,
-            title: .init(text: "\(application.localizedDisplayName!) blocked by ScreenFit", color: titleColor),
-            subtitle:  .init(text: "All apps in the \(category.localizedDisplayName!) category are blocked. Open ScreenFit and do some exercise to unblock.", color: subtitleColor),
+            title: .init(text: "\(application.localizedDisplayName!) blocked by ScreenFit", color: textColor),
+            subtitle:  .init(text: "All apps in the \(category.localizedDisplayName!) category are blocked. Open ScreenFit and do some exercise to unblock.", color: textColor),
             primaryButtonLabel: .init(text: "OK", color: primaryButtonLabelColor),
-            primaryButtonBackgroundColor: primaryButtonBackgroundColor,
-            secondaryButtonLabel: nil
+            primaryButtonBackgroundColor: primaryButtonBackgroundColor
         )
     }
     
@@ -53,13 +47,11 @@ class ShieldConfigurationExtension: ShieldConfigurationDataSource {
         // Customize the shield as needed for web domains.
         ShieldConfiguration(
             backgroundBlurStyle: backgroundBlurStyle,
-            backgroundColor: backgroundColor,
             icon: icon,
-            title: .init(text: "\(webDomain.domain!) blocked by ScreenFit", color: titleColor),
-            subtitle:  .init(text: "Open ScreenFit and do some exercise to unblock.", color: subtitleColor),
+            title: .init(text: "\(webDomain.domain!) blocked by ScreenFit", color: textColor),
+            subtitle:  .init(text: "Open ScreenFit and do some exercise to unblock.", color: textColor),
             primaryButtonLabel: .init(text: "OK", color: primaryButtonLabelColor),
-            primaryButtonBackgroundColor: primaryButtonBackgroundColor,
-            secondaryButtonLabel: nil
+            primaryButtonBackgroundColor: primaryButtonBackgroundColor
         )
     }
     
@@ -67,13 +59,11 @@ class ShieldConfigurationExtension: ShieldConfigurationDataSource {
         // Customize the shield as needed for web domains shielded because of their category.
         ShieldConfiguration(
             backgroundBlurStyle: backgroundBlurStyle,
-            backgroundColor: backgroundColor,
             icon: icon,
-            title: .init(text: "\(webDomain.domain!) blocked by ScreenFit", color: titleColor),
-            subtitle:  .init(text: "All websites in the \(category.localizedDisplayName!) category are blocked. Open ScreenFit and do some exercise to unblock.", color: subtitleColor),
+            title: .init(text: "\(webDomain.domain!) blocked by ScreenFit", color: textColor),
+            subtitle:  .init(text: "All websites in the \(category.localizedDisplayName!) category are blocked. Open ScreenFit and do some exercise to unblock.", color: textColor),
             primaryButtonLabel: .init(text: "OK", color: primaryButtonLabelColor),
-            primaryButtonBackgroundColor: primaryButtonBackgroundColor,
-            secondaryButtonLabel: nil
+            primaryButtonBackgroundColor: primaryButtonBackgroundColor
         )
     }
 }
